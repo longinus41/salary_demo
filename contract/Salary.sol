bb229dfce4ad8dc980cb2bb9f1d473f0938f5b68
longinus41 <longinus41@gmail.com>
2020-03-08 19:37:45 +0800

    add modExpQuick()

--- contract/Salary.sol
+++ contract/Salary.sol
@@ -142,6 +142,33 @@ contract Salary {
 		return result;
 	}
 
+	//A more gas friendly method of modular exponentiation using precompiled contract in Ethereum. Only on Ethereum full node.
+	function modExpQuick(uint256 _b, uint256 _e, uint256 _m) public returns (uint256 result) {
+        assembly {
+            // Free memory pointer
+            let pointer := mload(0x40)
+
+            // Define length of base, exponent and modulus. 0x20 == 32 bytes
+            mstore(pointer, 0x20)
+            mstore(add(pointer, 0x20), 0x20)
+            mstore(add(pointer, 0x40), 0x20)
+
+            // Define variables base, exponent and modulus
+            mstore(add(pointer, 0x60), _b)
+            mstore(add(pointer, 0x80), _e)
+            mstore(add(pointer, 0xa0), _m)
+
+            // Store the result
+            let value := mload(0xc0)
+
+            // Call the precompiled contract 0x05 = bigModExp
+            if iszero(call(not(0), 0x05, 0, pointer, 0xc0, value, 0x20)) {
+                revert(0, 0)
+            }
+            result := mload(value)
+        }
+    }
+
 	//Calculate the modular multiplicative invserse of x using Fermat's Little Theorem
 	function modInverse(uint256 _g, uint256 _n) internal pure returns (uint256){
 		return modExp(_g, _n-2, _n);
